b938bd27db3238b3f4b6cb9643f5965511afad24
mohammed <mooodo150@gmail.com>
2026-09-01 14:32:59 +0400

    fate: I Use Lombok for getters and setters

--- library-api/src/main/java/com/example/library_api/entity/Book.java
+++ library-api/src/main/java/com/example/library_api/entity/Book.java
@@ -1,5 +1,26 @@
 package com.example.library_api.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean IsActive;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
 
 }
